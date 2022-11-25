7e4da33cdea2e51ed72c8cee6a872257b3ef2415
Vishnuvardhan EV <vishnuvardhanev369@gmail.com>
2022-11-25 10:16:16 +0530

    adding todo app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState,useRef ,useEffect} from "react";
+import "./App.css"
+
+const App = () => {
+ 
+  const [search, setSearch] = useState("");
+
+  const input = useRef(null);
+
+  useEffect(() => {
+    input.current.focus()
+  }, [])
+
+  const updateSearch = (e) => {
+    setSearch(input.current.value);
+    console.log(search);
+  };
+  const [list, setList] = useState([]);
+  const updateList = () => {
+    setList([...list, search]);
+  };
+
+  return (
+    <div>
+      <h1>Welcome  todolist</h1>
+      
+      <input type="text" onChange={updateSearch} ref={input} />
+      <button onClick={updateList}>enter</button>
+      
+      {list.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
